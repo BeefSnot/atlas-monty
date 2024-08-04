721aeaedb0450c6784b579dc2e26e210734f079d
James <jameshamby2@icloud.com>
2024-08-03 22:56:25 -0500

    added stackops

--- stackops.c
+++ stackops.c
@@ -6,32 +6,43 @@
  * @ln: line num
  * Return: void
  */
-
 void push(stack_t **h, __attribute((unused))unsigned int ln)
 {
-	stack_t *new;
+    stack_t *new;
 
-	new = malloc(sizeof(stack_t));
-	if (new == NULL)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
-		exit(37);
-	}
+    new = malloc(sizeof(stack_t));
+    if (new == NULL)
+    {
+        fprintf(stderr, "Error: malloc failed\n");
+        exit(37);
+    }
 
-	new->prev = NULL;
+    new->prev = NULL;
 
-	if (*h == NULL)
-	{
-		new->next = NULL;
-	}
-	else
-	{
-		(*h)->prev = new;
-		new->next = *h;
-	}
-	*h = new;
+    if (*h == NULL)
+    {
+        new->next = NULL;
+    }
+    else
+    {
+        (*h)->prev = new;
+        new->next = *h;
+    }
+    *h = new;
 }
 
+/**
+ * pall_recursive - helper function to print every value in a stack recursively
+ * @node: current node in the stack
+ */
+void pall_recursive(stack_t *node)
+{
+    if (node == NULL)
+        return;
+
+    printf("%d\n", node->n);
+    pall_recursive(node->next);
+}
 
 /**
  * pall - prints every value in a stack
@@ -39,37 +50,27 @@ void push(stack_t **h, __attribute((unused))unsigned int ln)
  * @ln: line number
  * Return: void
  */
-
 void pall(stack_t **h, __attribute__((unused))unsigned int ln)
 {
-	stack_t *temp = *h;
-
-	while (temp)
-	{
-		printf("%d\n", temp->n);
-		temp = temp->next;
-	}
+    pall_recursive(*h);
 }
-
 
 /**
  * pint - prints a value in the head of stack
- * * @h: head of node
+ * @h: head of node
  * @ln: line number
  * Return: void
  */
-
 void pint(stack_t **h, unsigned int ln)
 {
-	if (*h == NULL)
-	{
-		fprintf(stderr, "L%d: can't pint, stack empty\n", ln);
-		isFail = 1;
-		return;
-	}
-	printf("%d\n", (*h)->n);
+    if (*h == NULL)
+    {
+        fprintf(stderr, "L%d: can't pint, stack empty\n", ln);
+        isFail = 1;
+        return;
+    }
+    printf("%d\n", (*h)->n);
 }
-
 
 /**
  * pop - pop! head
@@ -77,22 +78,19 @@ void pint(stack_t **h, unsigned int ln)
  * @ln: line number
  * Return: void
  */
-
 void pop(stack_t **h, unsigned int ln)
 {
-	stack_t *temp = *h;
+    stack_t *temp = *h;
 
-	if (*h == NULL)
-	{
-		fprintf(stderr, "L%d: can't pop an empty stack\n", ln);
-		isFail = 1;
-		return;
-	}
-	*h = (*h)->next;
-	free(temp);
-
+    if (*h == NULL)
+    {
+        fprintf(stderr, "L%d: can't pop an empty stack\n", ln);
+        isFail = 1;
+        return;
+    }
+    *h = (*h)->next;
+    free(temp);
 }
-
 
 /**
  * swap - swaps the top recent nodes on a stack
@@ -100,21 +98,19 @@ void pop(stack_t **h, unsigned int ln)
  * @ln: line number
  * Return: void
  */
-
 void swap(stack_t **h, unsigned int ln)
 {
-	stack_t *temp;
-	int x;
+    stack_t *temp;
+    int x;
 
-	if (*h == NULL || (*h)->next == NULL)
-	{
-		fprintf(stderr, "L%d: can't swap, stack too short\n", ln);
-		isFail = 1;
-		return;
-	}
-	temp = (*h)->next;
-	x = (*h)->n;
-	(*h)->n = temp->n;
-	temp->n = x;
-
+    if (*h == NULL || (*h)->next == NULL)
+    {
+        fprintf(stderr, "L%d: can't swap, stack too short\n", ln);
+        isFail = 1;
+        return;
+    }
+    temp = (*h)->next;
+    x = (*h)->n;
+    (*h)->n = temp->n;
+    temp->n = x;
 }
